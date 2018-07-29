33f487796bb6f2b799d433dae9e912032de4b99b
Fernando <fernandolgccastro@gmail.com>
2018-07-29 19:45:37 -0300

    Teste de commit/pull

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # celbux
 Repositório para desenvolvimento da integração COFA com a Celbux
+
+Documentações integrantes:
+
+https://codex.wordpress.org/Developer_Documentation#Plugin_Development
+
+https://docs.woocommerce.com/document/payment-gateway-api/
---
+++ agent-celbux.php
@@ -0,0 +1,14 @@
+defined( 'ABSPATH' ) or die( 'Proteção contra injeções de código!' );
+<?php
+/*
+Plugin Name:  Agent TI Wordpress Celbux WooCommerce Gateway
+Plugin URI:   https://www.agentti.com.br/celbux
+Description:  Plugin desenvolvido para integrar o método de pagamento Celbux com a ferramenta WooCommerce
+Version:      0.1
+Author:       Agent TI
+Author URI:   https://www.agentti.com.br/celbux
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  wporg
+Domain Path:  /languages
+*/
